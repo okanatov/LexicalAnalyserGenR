751a98446bb83298c594b0ada3ff8fd06c8a019a
Oleg Kanatov <okanatov@gmail.com>
2015-12-07 20:35:16 +0400

    Tree is created from the string representation
    Signed-off-by: Oleg Kanatov <okanatov@gmail.com>

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,53 +1,82 @@
+class Node
+    attr_reader :left, :right
+
+    def initialize(data, left, right)
+        @data = data
+        @left = left
+        @right = right
+    end
+
+    def to_s
+        "Node: data=#{@data}, left=#{@left}, right=#{@right}"
+    end
+end
+
 class Tree
     def initialize(string)
         @string = string
         @lookahead = @string.getc
+
         while @lookahead =~ /[[:blank:]]/
             @lookahead = @string.getc
         end
     end
 
     def parse()
-        expr()
+        @tree = expr()
+        tree_print(@tree)
+    end
+
+    private
+
+    def tree_print(node)
+        if node == nil
+            return
+        end
+
+        tree_print(node.left)
+        puts node
+        tree_print(node.right)
     end
 
     def expr()
-        result = term()
+        left = term()
         while true do
             if @lookahead == '+'
                 match('+')
-                result += term()
-            elsif @lookahead == '-'
-                match('-')
-                result -= term()
+                right = term()
+
+                left = Node.new('+', left, right)
             else
                 break
             end
         end
-        return result
+
+        return left
     end
 
     def term()
-        result = factor()
+        left = factor()
         while true do
             if @lookahead == '*'
                 match('*')
-                result *= factor()
-            elsif @lookahead == '/'
-                match('/')
-                result /= term()
+                right = factor()
+
+                left = Node.new('*', left, right)
             else
                 break
             end
         end
-        return result
+
+        return left
     end
 
     def factor()
         if @lookahead =~ /[[:digit:]]/
-            temp = @lookahead
+            node = Node.new(@lookahead, nil, nil)
             match(@lookahead)
-            return temp.to_i
+
+            return node
         else
             raise IOError
         end
@@ -56,6 +85,7 @@ class Tree
     def match(char)
         if @lookahead == char
             @lookahead = @string.getc
+
             while @lookahead =~ /[[:blank:]]/
                 @lookahead = @string.getc
             end
--- test/test_tree.rb
+++ test/test_tree.rb
@@ -5,6 +5,6 @@ require_relative '../lib/tree'
 class SimpleTree < MiniTest::Test
     def test_simple_tree
         tree = Tree.new(StringIO.new("2 + 3 * 5"))
-        assert_equal(2 + 3 * 5, tree.parse())
+        tree.parse()
     end
 end
