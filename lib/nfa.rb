a77f70a920017a0f82432947ac4ee7c07adadf45
Oleg Kanatov <okanatov@gmail.com>
2016-07-20 16:05:48 +0300

    Re-factoring
    Signed-off-by: Oleg Kanatov <okanatov@gmail.com>

--- lib/nfa.rb
+++ lib/nfa.rb
@@ -10,7 +10,7 @@ include Graph
 # Creates an NFA from a string passed in the constructor
 # and checks whether the NFA matches a given string.
 class NFA
-  attr_reader :end
+  attr_reader :size
 
   def self.from_string(string)
     parser = RegexpParser.new(StringIO.new(string))
@@ -18,24 +18,43 @@ class NFA
     new(node.build)
   end
 
-  def matches?(string, method = :breadth)
-    @found = false
-    @end = 0
+  def matches?(string)
+    @old_states = []
+    @new_states = []
+    @states = []
 
-    (0..string.length - 1).each do |i|
-      @end = i
-      if method == :breadth
-        io = StringIO.new(string[i..string.length])
-        breadth_search(io)
-      elsif method == :depth
-        depth_search(0, string[i..string.length], @end)
-      else
-        break
+    add_state(@old_states, 0)
+    @states << @old_states.clone
+
+    until string.eof?
+      char = string.getc
+
+      @old_states.each do |state|
+        next_states = move(state, char)
+        next_states.each { |s| add_state(@new_states, s) unless @new_states.include?(s) }
       end
-      break if @found
+
+      break if @new_states.empty?
+
+      @states << @new_states.clone
+
+      @old_states.clear
+      @old_states = @new_states.clone
+      @new_states.clear
     end
 
-    @found
+    @states.reverse!
+
+    @states.each_index do |index|
+      @states[index].each do |state|
+        if @graph.final?(state)
+          @size = @states.size - 1 - index
+          return true
+        end
+      end
+    end
+
+    false
   end
 
   def to_s
@@ -46,58 +65,6 @@ class NFA
 
   def initialize(graph)
     @graph = graph
-  end
-
-  def breadth_search(stringio)
-    @old_states = []
-    @new_states = []
-    @dfa_states = []
-
-    add_state(@old_states, 0)
-    @dfa_states << @old_states.clone
-
-    until stringio.eof?
-      char = stringio.getc
-      @old_states.each do |state|
-        next_states = move(state, char)
-        next_states.each { |s| add_state(@new_states, s) unless @new_states.include?(s) }
-      end
-
-      break if @new_states.empty?
-      @dfa_states << @new_states.clone
-
-      @old_states, @new_states = @new_states, @old_states
-      @new_states.clear
-    end
-
-    dfa_states_len = calculate_states
-    @end = @end + dfa_states_len - 1
-  end
-
-  def calculate_states
-    states_len = @dfa_states.length
-    @dfa_states.reverse_each do |e|
-      e.each { |s| @found = true if @graph.final?(s) }
-      break if @found
-      states_len -= 1
-    end
-    states_len - 1
-  end
-
-  def depth_search(state, string, pos)
-    if @graph.final?(state)
-      @found = true
-      @end = pos - 1
-    else
-      @graph.each(state) do |k, v|
-        if k == string[0]
-          depth_search(v, string[1..string.length], pos.succ)
-        elsif k == :empty
-          depth_search(v, string, pos)
-        end
-        break true if @found
-      end
-    end
   end
 
   def add_state(array, state)
--- test/test_nfa.rb
+++ test/test_nfa.rb
@@ -13,89 +13,61 @@ class SimpleNFA < MiniTest::Test
   def setup
     @nfa_simple = NFA.from_string('abc')
     assert(nil != @nfa_simple)
+
+    @nfa_alternation_1 = NFA.from_string('a(b|c)d')
+    assert(nil != @nfa_alternation_1)
+
+    @nfa_alternation_2 = NFA.from_string('ab|cd')
+    assert(nil != @nfa_alternation_2)
   end
 
-  def test_does_not_match
-    refute(@nfa_simple.matches?('def'), "NFA matches \"abc\"")
-    refute(@nfa_simple.matches?('def', :depth), "NFA matches \"abc\"")
+  def test_does_not_match_def
+    refute(@nfa_simple.matches?(StringIO.new("def")), "NFA does match \"def\"")
   end
 
-  def test_simple_apply_abcdef
-    assert(@nfa_simple.matches?('abcdef'), "NFA cannot match \"abc\"")
-    assert_equal(2, @nfa_simple.end)
-
-    assert(@nfa_simple.matches?('abcdef', :depth), "NFA cannot match \"abc\"")
-    assert_equal(2, @nfa_simple.end)
+  def test_does_not_match_dbabc
+    refute(@nfa_simple.matches?(StringIO.new('dbabc')), "NFA does match \"dbabc\"")
   end
 
-  def test_simple_apply_dabcef
-    assert(@nfa_simple.matches?('dabcef'), "NFA cannot match \"abc\"")
-    assert_equal(3, @nfa_simple.end)
-
-    assert(@nfa_simple.matches?('dabcef', :depth), "NFA cannot match \"abc\"")
-    assert_equal(3, @nfa_simple.end)
+  def test_does_not_match_dabcef
+    refute(@nfa_simple.matches?(StringIO.new('dabcef')), "NFA does match \"dabcef\"")
   end
 
-  def test_simple_apply_dbabc
-    assert(@nfa_simple.matches?('dbabc'), "NFA cannot match \"abc\"")
-    assert_equal(4, @nfa_simple.end)
-
-    assert(@nfa_simple.matches?('dbabc', :depth), "NFA cannot match \"abc\"")
-    assert_equal(4, @nfa_simple.end)
+  def test_matches_abc
+    assert(@nfa_simple.matches?(StringIO.new("abc")), "NFA does not match \"abc\"")
+    assert_equal(3, @nfa_simple.size)
   end
 
-  def test_simple_apply_alternation_abd
-    @nfa_alternation = NFA.from_string('a(b|c)d')
-    assert(nil != @nfa_alternation)
-
-    assert(@nfa_alternation.matches?('abd'), "NFA cannot match \"abd\"")
-    assert_equal(2, @nfa_alternation.end)
-
-    assert(@nfa_alternation.matches?('abd', :depth), "NFA cannot match \"abd\"")
-    assert_equal(2, @nfa_alternation.end)
+  def test_matches_abcdef
+    assert(@nfa_simple.matches?(StringIO.new('abcdef')), "NFA does not match \"abcdef\"")
+    assert_equal(3, @nfa_simple.size)
   end
 
-  def test_simple_apply_alternation_acd
-    @nfa_alternation = NFA.from_string('a(b|c)d')
-    assert(nil != @nfa_alternation)
-
-    assert(@nfa_alternation.matches?('acd'), "NFA cannot match \"acd\"")
-    assert_equal(2, @nfa_alternation.end)
-
-    assert(@nfa_alternation.matches?('acd', :depth), "NFA cannot match \"acd\"")
-    assert_equal(2, @nfa_alternation.end)
+  def test_alternation_1_matches_abd
+    assert(@nfa_alternation_1.matches?(StringIO.new('abd')), "NFA does not match \"abd\"")
+    assert_equal(3, @nfa_alternation_1.size)
   end
 
-  def test_simple_apply_alternation_ab
-    @nfa_alternation = NFA.from_string('ab|cd')
-    assert(nil != @nfa_alternation)
-
-    assert(@nfa_alternation.matches?('ab'), "NFA cannot match \"ab\"")
-    assert_equal(1, @nfa_alternation.end)
-
-    assert(@nfa_alternation.matches?('ab', :depth), "NFA cannot match \"ab\"")
-    assert_equal(1, @nfa_alternation.end)
+  def test_alternation_1_matches_acd
+    assert(@nfa_alternation_1.matches?(StringIO.new('acd')), "NFA does not match \"acd\"")
+    assert_equal(3, @nfa_alternation_1.size)
   end
 
-  def test_simple_apply_alternation_cd
-    @nfa_alternation = NFA.from_string('ab|cd')
-    assert(nil != @nfa_alternation)
+  def test_alternation_2_ab
+    assert(@nfa_alternation_2.matches?(StringIO.new('ab')), "NFA does not match \"ab\"")
+    assert_equal(2, @nfa_alternation_2.size)
+  end
 
-    assert(@nfa_alternation.matches?('cd'), "NFA cannot match \"cd\"")
-    assert_equal(1, @nfa_alternation.end)
-
-    assert(@nfa_alternation.matches?('cd', :depth), "NFA cannot match \"cd\"")
-    assert_equal(1, @nfa_alternation.end)
+  def test_alternation_2_cd
+    assert(@nfa_alternation_2.matches?(StringIO.new('cd')), "NFA does not match \"cd\"")
+    assert_equal(2, @nfa_alternation_2.size)
   end
 
   def test_longest
-    @nfa_alternation = NFA.from_string('long|longest')
-    assert(nil != @nfa_alternation)
+    nfa_alternation = NFA.from_string('long|longest')
+    assert(nil != nfa_alternation)
 
-    assert(@nfa_alternation.matches?('longest'), "NFA cannot match \"longest\"")
-    assert_equal(6, @nfa_alternation.end)
-
-    assert(@nfa_alternation.matches?('longest', :depth), "NFA cannot match \"longest\"")
-    assert_equal(3, @nfa_alternation.end)
+    assert(nfa_alternation.matches?(StringIO.new('longest')), "NFA does not match \"longest\"")
+    assert_equal(7, nfa_alternation.size)
   end
 end
